159111d42968a292f64badf9a76a985d51467500
wudandangit <1058057286@qq.com>
2019-02-12 20:49:20 +0800

    将mint-ui更改为mui

--- vue-cms/src/main.js
+++ vue-cms/src/main.js
@@ -10,10 +10,10 @@ import Vueaxios from 'vue-axios'
 Vue.use(Vueaxios, axios)
 
 // 引入的css
-import './lib/mint-ui/css/mui.css'
+import './lib/mui/css/mui.css'
 //初始化样式的css
-import './lib/mint-ui/css/common.css'
-import './lib/mint-ui/css/icons-extra.css'
+import './lib/mui/css/common.css'
+import './lib/mui/css/icons-extra.css'
 
 // 引入顶部的组件
 // import { Header } from 'mint-ui';
