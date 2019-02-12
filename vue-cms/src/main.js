bf5713639f3d719e949063a4f05787ee94f3fdf9
wudandangit <1058057286@qq.com>
2019-02-12 20:42:56 +0800

    首页部分

---
+++ vue-cms/src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import App from './App'
+import router from './router'
+
+import axios from 'axios'
+import Vueaxios from 'vue-axios'
+
+Vue.use(Vueaxios, axios)
+
+// 引入的css
+import './lib/mint-ui/css/mui.css'
+//初始化样式的css
+import './lib/mint-ui/css/common.css'
+import './lib/mint-ui/css/icons-extra.css'
+
+// 引入顶部的组件
+// import { Header } from 'mint-ui';
+// Vue.component(Header.name, Header);
+
+Vue.use(MintUI)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
---
+++ vue-cms/src/pages/shopcar/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h3>shopcarComponents</h3>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
